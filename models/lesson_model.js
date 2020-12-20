10c7e27455deb2e4b78c89cbb94f8465554d17ff
zm412 <zm412412@gmail.com>
2020-12-20 16:23:30 -0500

    fixed sql

--- models/lesson_model.js
+++ models/lesson_model.js
@@ -147,7 +147,11 @@ const filteringFunc = (body) => {
     let offset = lessonsPerPage * page - lessonsPerPage;
 
     let sqlCommPart = `
-
+    
+CREATE TEMP TABLE temp_store ON COMMIT DROP
+AS
+    SELECT * FROM
+    (
       select DISTINCT ON (lessons.id) lessons.id,lessons.date, lessons.title,lessons.status,
           (
           select array_agg(row_to_json(d))
@@ -176,10 +180,45 @@ const filteringFunc = (body) => {
 
           
           from lessons, lesson_teachers  
+       ) AS combined;
+
+    select * from temp_store
+
     `
     
     let sqlFilters = ` 
-
+    
+    CREATE TEMP TABLE temp_store ON COMMIT DROP
+    AS
+      SELECT * FROM
+      (
+      select DISTINCT ON (lessons.id) lessons.id,lessons.date, lessons.title,lessons.status,
+          (
+          select array_agg(row_to_json(d))
+          from (
+              select students.id, students.name, lesson_students.visit
+              from students, lesson_students
+              where lesson_students.lesson_id = lessons.id AND
+              lesson_students.student_id = students.id
+                ) d
+          ) as students,
+           (
+          select array_agg(row_to_json(p))
+          from (
+              select teachers.id, teachers.name
+              from teachers, lesson_teachers
+              where lesson_teachers.lesson_id = lessons.id AND
+              lesson_teachers.teacher_id = teachers.id
+                ) p
+          ) as teachers,
+            (
+              select count(*)
+              from lesson_students
+              where lesson_students.visit = 't' AND
+              lesson_students.lesson_id = lessons.id
+          ) as visitCount 
+          
+        from lessons, lesson_teachers  
 
 
       WHERE 
@@ -199,10 +238,13 @@ const filteringFunc = (body) => {
             where
             lesson_students.lesson_id = lessons.id
         ) IN  (  ${studentsCount} ) 
+       ) AS combined ;
+
+    select * from temp_store
 
     `;
 
-    let sqlFullQuery = body.date === '' && body.status === '' && body.teacherIds === '' && body.studentsCount === '' ? sqlCommPart : sqlCommPart + sqlFilters;
+    let sqlFullQuery = body.date === '' && body.status === '' && body.teacherIds === '' && body.studentsCount === '' ? sqlCommPart : sqlFilters;
     
 
     
@@ -213,8 +255,8 @@ const filteringFunc = (body) => {
                 console.log(error)
                 reject(error)
               }
-      console.log('RESULT INDEX',results.rows)
-      resolve(results.rows);
+      console.log('RESULT INDEX',results[1].rows)
+      resolve(results[1].rows);
     })
   })
 }
